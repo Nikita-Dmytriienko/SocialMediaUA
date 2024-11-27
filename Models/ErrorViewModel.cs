5bbb70739ffac02bcd22de7ab196ca1440424c55
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-11-27 17:14:28 +0200

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialMediaUA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
