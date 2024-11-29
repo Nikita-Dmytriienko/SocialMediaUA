e3ab6ceaf6b47957bdd120e96d9b6499cf7250a7
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-11-29 18:36:03 +0200

    delete "Models" folder

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SocialMediaUA.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
