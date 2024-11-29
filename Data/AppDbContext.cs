e4e96a288a903119396077a1a4b157d6e35f620c
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-11-29 18:42:20 +0200

    added DbSet

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using SocialMediaUA.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SocialMediaUA.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext: DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
+
+
+        public DbSet<Post> Posts { get; set; }
     }
 }
---
+++ Data/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaUA.Data.Models
+{
+
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        public string Content { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public int NrOfReports {  get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+    }
+}
